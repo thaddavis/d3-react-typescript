036dc0214aa168d33df9106706f33b817d1c8b4f
a <a@a.com>
2023-04-05 14:45:20 -0400

    end Part 1 of Video Series follow-along

--- src/components/1.tsx
+++ src/components/1.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { select } from "d3-selection";
+import React, { useEffect, useRef } from "react";
 
-export const _1 = () => {
-  return <div>_1</div>;
+export const _1: React.FC = () => {
+  const svgRef = useRef<SVGSVGElement | null>(null);
+
+  useEffect(() => {
+    select(svgRef.current)
+      .append("rect")
+      .attr("width", 100)
+      .attr("height", 100)
+      .attr("fill", "green");
+  }, []);
+
+  return (
+    <div>
+      <svg ref={svgRef}></svg>
+    </div>
+  );
 };
--- src/components/2.tsx
+++ src/components/2.tsx
@@ -1,5 +1,36 @@
-import React from "react";
+import { select, selectAll } from "d3-selection";
+import React, { useEffect, useRef } from "react";
 
-export const _2 = () => {
-  return <div>_2</div>;
+export const _2: React.FC = () => {
+  const svgRef = useRef<SVGSVGElement | null>(null);
+
+  useEffect(() => {
+    select(svgRef.current)
+      .append("rect")
+      .attr("width", 100)
+      .attr("height", 100)
+      .attr("fill", "green");
+
+    //   selectAll('rect')
+    //   selectAll('.foo')
+    //   selectAll('#foo')
+
+    selectAll("rect")
+      .attr("width", 100)
+      .attr("height", 100)
+      .attr("fill", "blue")
+      .attr("x", (_, i) => {
+        return i * 100;
+      });
+  }, []);
+
+  return (
+    <div>
+      <svg ref={svgRef}>
+        <rect className="foo" />
+        <rect id="foo" />
+        <rect />
+      </svg>
+    </div>
+  );
 };
